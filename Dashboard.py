f76a28d78ebf089c2ab7a165bce7627d000d32e3
fabienfh <59026618+fabienfh@users.noreply.github.com>
2022-11-27 11:22:41 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -17,7 +17,8 @@ import plotly.graph_objects as go
 import shap
 
 # URL API : local et cloud
-URL_API = "http://127.0.0.1:5000/"
+#URL_API = "http://127.0.0.1:5000/"
+URL_API = 'https://fabienfh-openclassroomsp7-dashboard-wo11ni.streamlit.app/'
 #URL_API = 'https://fabien-haffar-p7.herokuapp.com/'
 
 st.set_page_config(layout="wide") # Affichage large
